55216dcc0c917d4a8f141ae6cf9d05d31844ec22
museop <kms920806@naver.com>
2018-05-23 23:49:51 +0900

    update DeliveryDAO class

--- DAOs/DeliveryDAO.py
+++ DAOs/DeliveryDAO.py
@@ -2,12 +2,11 @@ from DAOs import db
 
 
 class DeliveryDAO:
-    def __init__(self, order_id):
+    def __init__(self):
         self.db = db.get_db()
-        self.order_id = order_id
 
     def __enter__(self):
         return self
 
-    def get_delivery(self):
-        return self.db.execute('SELECT * FROM delivery WHERE order_id=?', (self.order_id,)).fetchone()
+    def get_delivery(self, order_id):
+        return self.db.execute('SELECT * FROM delivery WHERE order_id=?', (order_id,)).fetchone()
