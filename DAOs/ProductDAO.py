5524b541875d0af5a58b2258369c3cf65eb5c78a
estherlee2608 <39487159+estherlee2608@users.noreply.github.com>
2018-05-23 22:43:19 +0900

    Add files via upload

---
+++ DAOs/ProductDAO.py
@@ -0,0 +1,22 @@
+from DAOs import db
+
+
+class ProductDAO:
+    def __init__(self, product_id):
+        self.db = db.get_db()
+        self.product_id = product_id
+
+    def __enter__(self):
+        return self
+
+	def getDc_rate(self):
+		return self.db.execute('SELECT * FROM product ORDER BY dc_rate DESC LIMIT 3', (self.product_id,)).fetchmany(3)
+		
+	def getSales_num(self):
+		return self.db.execute('SELECT * FROM product ORDER BY sales_num DESC LIMIT 3', (self.product_id,)).fetchmany(3)
+
+	def getCategory(self):
+		return self.db.execute('SELECT * FROM product WHERE category = ?', (self.product_id,)).fetchall()
+		
+	def getName(self):
+		return self.db.execute('SELECT * FROM product WHERE name = ?', (self.product_id,)).fetchone()
