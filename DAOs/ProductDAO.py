aed1636a932a7478fae59a3abaa4c73b4514e818
estherlee2608 <39487159+estherlee2608@users.noreply.github.com>
2018-05-23 22:50:55 +0900

    Update ProductDAO.py

--- DAOs/ProductDAO.py
+++ DAOs/ProductDAO.py
@@ -8,15 +8,15 @@ class ProductDAO:
 
     def __enter__(self):
         return self
-
-	def getDc_rate(self):
-		return self.db.execute('SELECT * FROM product ORDER BY dc_rate DESC LIMIT 3', (self.product_id,)).fetchmany(3)
+    
+    def getDc_rate(self):
+	return self.db.execute('SELECT * FROM product ORDER BY dc_rate DESC LIMIT 3', (self.product_id,)).fetchmany(3)
 		
-	def getSales_num(self):
-		return self.db.execute('SELECT * FROM product ORDER BY sales_num DESC LIMIT 3', (self.product_id,)).fetchmany(3)
+    def getSales_num(self):
+	return self.db.execute('SELECT * FROM product ORDER BY sales_num DESC LIMIT 3', (self.product_id,)).fetchmany(3)
 
-	def getCategory(self):
-		return self.db.execute('SELECT * FROM product WHERE category = ?', (self.product_id,)).fetchall()
+    def getCategory(self):
+	return self.db.execute('SELECT * FROM product WHERE category = ?', (self.product_id,)).fetchall()
 		
-	def getName(self):
-		return self.db.execute('SELECT * FROM product WHERE name = ?', (self.product_id,)).fetchone()
+    def getName(self):
+	return self.db.execute('SELECT * FROM product WHERE name = ?', (self.product_id,)).fetchone()
