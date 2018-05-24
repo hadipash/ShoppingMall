44ba345443ba01d5096b7a46f561e562eaaa1fea
museop <kms920806@naver.com>
2018-05-24 13:14:09 +0900

    update product_search

--- app.py
+++ app.py
@@ -38,9 +38,10 @@ def create_app(test_config=None):
     db.init_app(app)
 
     # apply the blueprints to the app
-    import auth, blog
+    import auth, blog, search
     app.register_blueprint(auth.bp)
     app.register_blueprint(blog.bp)
+    app.register_blueprint(search.bp)
 
     # make url_for('index') == url_for('blog.index')
     # in another app, you might define a separate main index here with
---
+++ search.py
@@ -0,0 +1,27 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+
+from DAOs.db import get_db
+
+
+bp = Blueprint('search', __name__, url_prefix='/search')
+
+
+@bp.route('/product_name', methods=('GET', 'POST'))
+def product_name():
+    if request.method == 'POST':
+        product_name = request.form['product_name']
+        db = get_db()
+        error = None
+
+        if not product_name:
+            error = 'Product name is required'
+        if error is None:
+            products = db.execute(
+                'SELECT * FROM product WHERE name = ?', (product_name,)
+            ).fetchall()
+            print(type(products))
+            print(len(products))
+            return render_template('search/product_name.html', products=products)
+    return render_template('search/product_name.html', products=None)
