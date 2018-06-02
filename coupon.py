7962f9b18a3253211cf8d16ddfbb7ff8d9cc46ee
jak5022 <jak5022@naver.com>
2018-06-02 18:41:07 +0900

    update 쿠폰추가화면

--- coupon.py
+++ coupon.py
@@ -10,17 +10,21 @@ bp = Blueprint('coupon', __name__, url_prefix='/coupon')
 
 @bp.route('/add_coupon', methods=['POST'])
 def add_coupon():
-    userid = 1
-
+    username = session.get('user_id')
     db = get_db()
+    selectcoupon = request.form['coupon_id']
+    mycoupon_list = db.execute('SELECT coupon_id FROM coupon_list WHERE user_id=?', [username]).fetchall()
+    coupon_name = db.execute('SELECT name FROM coupon WHERE coupon_id=?', [selectcoupon]).fetchone()
+    for coupon in mycoupon_list :
+        if int(coupon[0]) == int(request.form['coupon_id']):
+            flash(coupon_name[0] + '  쿠폰은 이미 가지고 있거나 사용하셨습니다.')
+            return redirect('coupon/coupon_list')
 
-    db.execute('DELETE FROM coupon_list')
-
-    db.execute('INSERT into coupon_list(user_id,coupon_id) values(?,?)',
-               [userid, request.form['coupon_id']])
+    db.execute('INSERT into coupon_list(user_id,coupon_id) values(?,?)',(username, selectcoupon))
     db.commit()
 
-    print('coupon ', request.form['coupon_id'], ' added!')
+    flash(coupon_name[0] + '  쿠폰을 획득하셨습니다.')
+    print('coupon', selectcoupon, ' added!')
     return redirect('coupon/coupon_list')
 @bp.route('/coupon_list')
 def coupon_list():
